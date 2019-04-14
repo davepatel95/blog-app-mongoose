92c2e058a41d5da54f3d6f3be5898aa8f1c6f96d
Dave Patel <davepatel95@gmail.com>
2019-04-13 20:16:31 -0700

    added 8080 to config

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 'use strict';
-exports.DATABASE_URL = process.env.DATABASE_URL || "mongodb://localhost/blog-app-mongoose";
+exports.DATABASE_URL = process.env.DATABASE_URL || "mongodb://localhost:8080/blog-app-mongoose";
 exports.PORT = process.env.PORT || 8080;
